af52383665ae73737fd2ef111084b311b41c76ba
FrankKai <gaokai20100801@gmail.com>
2019-03-18 16:09:04 +0800

    first commit;

---
+++ src/main/java/com/arya/spring/vue/aryaspringvuebe/AryaSpringVueBeApplication.java
@@ -0,0 +1,13 @@
+package com.arya.spring.vue.aryaspringvuebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryaSpringVueBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AryaSpringVueBeApplication.class, args);
+	}
+
+}
