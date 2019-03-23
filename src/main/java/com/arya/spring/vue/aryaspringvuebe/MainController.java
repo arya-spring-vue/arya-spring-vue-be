bc73f6d6c9cedc8775299225a5a0d23414124200
FrankKai <gaokai20100801@gmail.com>
2019-03-23 22:29:41 +0800

    更新create为POST；添加update PUT接口；

--- src/main/java/com/arya/spring/vue/aryaspringvuebe/MainController.java
+++ src/main/java/com/arya/spring/vue/aryaspringvuebe/MainController.java
@@ -2,10 +2,10 @@ package com.arya.spring.vue.aryaspringvuebe;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/spring/vue")
@@ -13,7 +13,7 @@ public class MainController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping(path = "/createUser")
+    @PostMapping(path = "/createUser")
     public @ResponseBody String createUser(@RequestParam String name, @RequestParam String sex, @RequestParam Integer age, @RequestParam String email){
         User user = new User();
         user.setName(name);
@@ -28,4 +28,24 @@ public class MainController {
     public @ResponseBody Iterable<User> readUsers() {
         return userRepository.findAll();
     }
+
+    // 通过id查找到user实例
+    public User getUserById(Integer id) {
+        // 根据id查询出对应的持久化对象
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+    // 按照这样Restful形式的PUT写接口，可以清晰地区分出PK和其它字段
+    @PutMapping(path = "/updateUser/{id}")
+    public @ResponseBody String updateUser(@PathVariable(value = "id") Integer id ,@Valid @RequestBody User userDetails){
+        // 通过@PathVariable传入关键id，通过@RequestBody传入具体更新数据
+        User user = getUserById(id);
+        user.setName(userDetails.getName());
+        user.setSex(userDetails.getSex());
+        user.setAge(userDetails.getAge());
+        user.setEmail(userDetails.getEmail());
+        userRepository.save(user);
+        return "更新用户成功！";
+    }
+
 }
