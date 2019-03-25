f17fa15932945077d68b36610d52002240fb566b
FrankKai <gaokai20100801@gmail.com>
2019-03-25 13:02:42 +0800

    /spring/vue/*->/spring/vue/**,支持带OPTIONS预检请求的PUT方法;

--- src/main/java/com/arya/spring/vue/aryaspringvuebe/AryaSpringVueBeApplication.java
+++ src/main/java/com/arya/spring/vue/aryaspringvuebe/AryaSpringVueBeApplication.java
@@ -18,9 +18,18 @@ public class AryaSpringVueBeApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/spring/vue/*") //映射项目路由，允许/spring/vue下的所有接口跨域
-                        .allowedOrigins("*") //允许所有源访问，后期可以添加指定源访问提升安全性
-                        .allowCredentials(true); // 设置响应Access-Control-Allow-Credentials: true
+                // 映射项目路由，允许/spring/vue下的所有接口跨域。注意**才是代表所有路由，否则*仅仅是类似/spring/vue/update一级路由，不支持/spring/vue/update?id=1。
+                registry.addMapping("/spring/vue/**")
+                        // 指定
+                        .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+                        // 设置Access-Control-Allow-Origin:http://localhost:3000，指定可访问源。注意PUT和DELETE引起的OPTIONS预检请求需要指定源，不可设置为*。
+                        .allowedOrigins("http://localhost:3000")
+                        // 设置Access-Control-Allow-Headers: content-type。
+                        .allowedHeaders("Content-Type")
+                        // 设置Access-Control-Max-Age: 3600。注意这里是Long类型的
+                        .maxAge(3600L)
+                        // 设置响应Access-Control-Allow-Credentials: true。
+                        .allowCredentials(true);
             }
         };
     }
